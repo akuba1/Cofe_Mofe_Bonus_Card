6deb967ba38f273f3013f5b6b33f29dedef8dc0c
Akuba <abilxairbars@gmail.com>
2025-07-30 12:16:26 +0500

    change purchase to remove !

--- api/purchase.js
+++ api/purchase.js
@@ -1,11 +1,15 @@
 import { createClient } from '@supabase/supabase-js';
 
 console.log("ENV SUPABASE_URL:", process.env.SUPABASE_URL);
-console.log("ENV SUPABASE_ANON_KEY:", process.env.SUPABASE_ANON_KEY?.slice(0, 10) + "…");
+console.log(
+  "ENV SUPABASE_ANON_KEY:",
+  process.env.SUPABASE_ANON_KEY
+    ? process.env.SUPABASE_ANON_KEY.slice(0, 10) + "…"
+);
 
 const supabase = createClient(
-  process.env.SUPABASE_URL!,
-  process.env.SUPABASE_ANON_KEY!
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 
 async function sendTelegramMessage(text) {
